be64be430038a7b25198015d3881b3a1b35c83d2
Beanca Camille Serdena <serdenabeanca@gmail.com>
2020-12-01 12:35:31 +0000

    User Story 1.1 - Build a database

---
+++ FilmsAndSeriesManagerModel/ShowStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FilmsAndSeriesManagerModel
+{
+    public partial class ShowStatus
+    {
+        public ShowStatus()
+        {
+            Shows = new HashSet<Show>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Show> Shows { get; set; }
+    }
+}
