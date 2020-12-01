8493a8499ef12b193b65ffcaa6102d5223f82618
Beanca Camille Serdena <serdenabeanca@gmail.com>
2020-12-01 12:55:07 +0000

    User Story 1.3 - Add a business layer

---
+++ FilmsAndSeriesManagerBusiness/Methods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmsAndSeriesManagerBusiness
+{
+    class Methods
+    {
+    }
+}
---
+++ FilmsAndSeriesManagerBusiness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilmsAndSeriesManagerBusiness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
